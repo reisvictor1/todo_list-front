325daede90793580b9359bf8dceedb02a46c6d6c
reisvictor1 <reisvictor448@gmail.com>
2019-07-29 15:58:24 -0300

    Funcionalidade de adicionar na lista adicionada

--- src/index.js
+++ src/index.js
@@ -1,12 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class List extends React.Component{
+
+
+ 
+    state = {
+        items: []
+    }
+
+
+    addItem = (props) => {
+
+       let newItem = document.getElementById('newItem');
+       let listItem = null;
+
+       if(newItem.value !== ""){
+
+            this.state.items.push(props.value)
+            console.log(this.state.items)
+
+            listItem = this.state.items.map(
+
+                (item) =>  <li>{item}</li>
+        
+            )
+
+        }else{
+            alert('Coloque um item na lista!')
+        }
+
+
+        newItem.value = null;
+        ReactDOM.render(listItem,document.getElementById('list'))
+    }
+
+    render(){
+        return(
+            <div>
+                <ul id='list' className='list'>  </ul>
+
+               
+                <label htmlFor='newItem'>Adicione tarefas abaixo:</label>
+                <input id='newItem'  type="text"></input>
+                <button onClick={() => this.addItem(document.getElementById('newItem'))} >Adicionar um elemento a lista</button>
+            </div>
+        )
+    }
+
+}
+
+class App extends React.Component{
+
+
+    render(){
+        return(
+            <main id="app">
+                <h3>Lista de coisas a fazer</h3>
+                <List />
+            </main>
+        )
+    }
+}
+
+
+
+//Call class to root
+
+ReactDOM.render(<App />, document.getElementById("root"))
